0aead7bf65b0ea04e2cc877b7947337a5f05c7bd
Nevroz Arslan <newrozarslan@gmail.com>
2024-02-13 22:10:17 +0300

    remove unnecerrary test

--- tree_test.go
+++ tree_test.go
@@ -2,23 +2,6 @@ package tree
 
 import "testing"
 
-func TestTree(t *testing.T) {
-
-	nt := New("A")
-	ntb := nt.AddTree("B")
-	ntb.AddTree("H")
-	ntb.AddTree("J")
-
-	ntc := nt.AddTree("C")
-	ntc.AddTree("D")
-	nte := ntc.AddTree("E")
-	ntc.AddTree("F")
-
-	nte.AddTree("G")
-
-	t.Log(nt)
-}
-
 func TestTreeTraverse(t *testing.T) {
 
 	nt := New("A")
